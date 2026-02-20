b5a92a1a3fd26cb474d298c70a89715b81c67bdd
Riddhi <tbs.riddhi@gmail.com>
2026-02-20 17:24:53 +0530

    fix: Correct import path for CreateCollection component in CollectionsPage

--- app/(admin)/collections/create/page.js
+++ app/(admin)/collections/create/page.js
@@ -1,11 +1,9 @@
-
-import CreateCollection from '../components/CreateCollection';
+import CreateCollection from '@/components/common/collections/CreateCollection';
 
 export default function CollectionsPage() {
     return (
         <div className="container-fluid py-4">
             <CreateCollection />
-            
         </div>
     );
 }
