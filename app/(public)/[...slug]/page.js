5eab9eaa8fafc2224472ed104236b2004c97b847
Riddhi <tbs.riddhi@gmail.com>
2026-03-09 13:06:55 +0530

    feat(region): add RegionDetails component and update dynamic page handling

--- app/(public)/[...slug]/page.js
+++ app/(public)/[...slug]/page.js
@@ -1,14 +1,15 @@
 import { notFound } from 'next/navigation';
 import { resolveSlug } from '@/lib/api/public/countryapi';
 import CountryDetails from '@/components/common/country/CountryDetails';
+import RegionDetails from '@/components/common/region/RegionDetails';
 
 export default async function DynamicPage({ params }) {
     const { slug } = await params;
 
     const slugArray = slug || [];
 
-    // prepend slash for backend
-    const fullSlug = `/${slugArray.join('/')}`;
+    // "/angola/ambuila"
+    const fullSlug = '/' + slugArray.join('/');
 
     const result = await resolveSlug(fullSlug);
 
@@ -22,6 +23,11 @@ export default async function DynamicPage({ params }) {
     if (slugArray.length === 1 && data.geoType === 'country') {
         return <CountryDetails country={slugArray[0]} />;
     }
+
+    // REGION PAGE
+    // if (slugArray.length === 2 && data.geoType === 'region') {
+    //     return <RegionDetails country={slugArray[0]} region={slugArray[1]} />;
+    // }
 
     return notFound();
 }
---
+++ components/common/region/RegionDetails.jsx
@@ -0,0 +1,10 @@
+
+import CountryHeroSection from '@/components/sections/CountryHeroSection';
+
+export default async function RegionDetails() {
+    return (
+        <>
+            <CountryHeroSection />
+        </>
+    );
+}
