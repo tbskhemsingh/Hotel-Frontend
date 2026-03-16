6d39461e992245d279ceca6d24e90f654e01b8f8
Riddhi <tbs.riddhi@gmail.com>
2026-03-16 18:33:21 +0530

    comment the collectiondetail  component

--- app/(public)/[...slug]/page.js
+++ app/(public)/[...slug]/page.js
@@ -2,7 +2,6 @@ import { notFound } from 'next/navigation';
 import { resolveSlug } from '@/lib/api/public/countryapi';
 import CountryDetails from '@/components/common/country/CountryDetails';
 import RegionDetails from '@/components/common/region/RegionDetails';
-import CollectionDetails from '@/components/common/collections/CollectionDetails';
 
 export default async function DynamicPage({ params }) {
     const { slug } = await params;
