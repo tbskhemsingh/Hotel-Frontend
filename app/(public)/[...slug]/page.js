c50b7822e22d24f6473a1789fad58c1a323d4a5e
VrushikaM <tbs.vrushika@gmail.com>
2026-03-16 18:30:25 +0530

    rename entity type

--- app/(public)/[...slug]/page.js
+++ app/(public)/[...slug]/page.js
@@ -2,15 +2,14 @@ import { notFound } from 'next/navigation';
 import { resolveSlug } from '@/lib/api/public/countryapi';
 import CountryDetails from '@/components/common/country/CountryDetails';
 import RegionDetails from '@/components/common/region/RegionDetails';
+import CollectionDetails from '@/components/common/collections/CollectionDetails';
 
 export default async function DynamicPage({ params }) {
     const { slug } = await params;
-
     const slugArray = slug || [];
     const fullSlug = '/' + slugArray.join('/');
 
     const result = await resolveSlug(fullSlug);
-
     if (!result || result.status !== 'success') {
         return notFound();
     }
@@ -18,14 +17,19 @@ export default async function DynamicPage({ params }) {
     const data = result.data;
 
     // COUNTRY PAGE
-    if (slugArray.length === 1 && data.geoType === 'country') {
+    if (slugArray.length === 1 && data.entityType === 'country') {
         return <CountryDetails country={slugArray[0]} />;
     }
 
     // REGION PAGE
-    if (slugArray.length === 2 && data.geoType === 'region') {
+    if (slugArray.length === 2 && data.entityType === 'region') {
         return <RegionDetails country={slugArray[0]} region={slugArray[1]} params={params} />;
     }
+
+    // COLLECTION PAGE
+    // if (slugArray.length === 1 && data.entityType === 'collection') {
+    //     return <CollectionDetails collection={slugArray[0]} params={params} />;
+    // }
 
     return notFound();
 }
