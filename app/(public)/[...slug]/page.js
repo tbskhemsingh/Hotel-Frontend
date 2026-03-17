06460240d82510ea8ae9ca37a58f0123ae3d781e
VrushikaM <tbs.vrushika@gmail.com>
2026-03-17 18:44:49 +0530

    integrated the api into the rules tab in collection create/update, replacing static values with dynamic data

--- app/(public)/[...slug]/page.js
+++ app/(public)/[...slug]/page.js
@@ -2,7 +2,6 @@ import { notFound } from 'next/navigation';
 import { resolveSlug } from '@/lib/api/public/countryapi';
 import CountryDetails from '@/components/common/country/CountryDetails';
 import RegionDetails from '@/components/common/region/RegionDetails';
-import CollectionDetails from '@/components/common/collections/CollectionDetails';
 
 export default async function DynamicPage({ params }) {
     const { slug } = await params;
@@ -25,11 +24,6 @@ export default async function DynamicPage({ params }) {
     if (slugArray.length === 2 && data.entityType === 'region') {
         return <RegionDetails country={slugArray[0]} region={slugArray[1]} params={params} />;
     }
-
-    // COLLECTION PAGE
-    // if (slugArray.length === 1 && data.entityType === 'collection') {
-    //     return <CollectionDetails collection={slugArray[0]} params={params} />;
-    // }
 
     return notFound();
 }
--- components/common/collections/CreateCollection.jsx
+++ components/common/collections/CreateCollection.jsx
@@ -88,6 +88,7 @@ export default function CreateCollection({ collectionId: propCollectionId }) {
         slug: '',
         sourceId: null,
         geoNodeType: null,
+        geoNodeName: null,
         countryId: null,
         regionId: null,
         cityId: null,
